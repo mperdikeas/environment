2bce44d49be0fb804960f0eeb5908406bb12d6b0
mperdikeas <mperdikeas@gmail.com>
2013-01-04 11:06:17 +0200

    added Clojure libs and improved Clojure's repl.sh to include them in the classpath

--- .emacs.d/clojure/repl.sh
+++ .emacs.d/clojure/repl.sh
@@ -1,1 +1,2 @@
-java -jar ~/.emacs.d/clojure/clojure-1.4.0.jar
+#java -cp ~/environment/clojure-libs -jar ~/.emacs.d/clojure/clojure-1.4.0.jar
+java -cp $(echo $(echo ~)/.emacs.d/clojure/clojure-1.4.0.jar:$(echo ~)/environment/clojure-libs) clojure.main
