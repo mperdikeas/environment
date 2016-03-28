76eb2856abd667dbdafdfc26a1683605ed6afef0
mperdikeas <mperdikeas@gmail.com>
2016-03-28 12:53:51 +0300

    environment

--- repos-list.sh
+++ repos-list.sh
@@ -1,3 +1,3 @@
 declare -a repos=("tools" "java-mutil" "environment" "ocaml-mutil" "playground"
                   "mperdikeas.github.com" "archetypes" "esavo-reg-II" "clojure-mutil"
-                  "mant-tasks" "starjava" "zip-diff" "esac-rawdar" "euler-js" "archetype-react")
+                  "mant-tasks" "starjava" "zip-diff" "esac-rawdar" "euler-js")
