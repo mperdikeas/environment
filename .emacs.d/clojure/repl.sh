956653dcbe738c72da1507ba25cd74da5c361b2e
mperdikeas <mperdikeas@gmail.com>
2016-04-25 13:16:25 +0300

    emacs now using Clojure 1.8.0 for inferior Lisp

--- .emacs.d/clojure/repl.sh
+++ .emacs.d/clojure/repl.sh
@@ -4,7 +4,7 @@ FILE="./.clojure"
 
 if [ -f $FILE ];
 then
-    java -cp $(echo $(echo ~)/.emacs.d/clojure/clojure-1.4.0.jar:$(echo ~)/environment/clojure-libs):$(echo ./lib/*.jar | tr ' ' ':'):.:$(cat $FILE) clojure.main $*
+    java -cp $(echo $(echo ~)/.emacs.d/clojure/clojure-1.8.0.jar:$(echo ~)/environment/clojure-libs):$(echo ./lib/*.jar | tr ' ' ':'):.:$(cat $FILE) clojure.main $*
 else
-    java -cp $(echo $(echo ~)/.emacs.d/clojure/clojure-1.4.0.jar:$(echo ~)/environment/clojure-libs):$(echo ./lib/*.jar | tr ' ' ':'):. clojure.main $*
+    java -cp $(echo $(echo ~)/.emacs.d/clojure/clojure-1.8.0.jar:$(echo ~)/environment/clojure-libs):$(echo ./lib/*.jar | tr ' ' ':'):. clojure.main $*
 fi
