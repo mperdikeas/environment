a8cc9c12abc1df8f66928e91de354a8ea59a49e2
mperdikeas <mperdikeas@gmail.com>
2016-06-20 15:56:34 +0300

    added some new projects

--- repos-list.sh
+++ repos-list.sh
@@ -1,3 +1,6 @@
 declare -a repos=("tools" "java-mutil" "environment" "ocaml-mutil" "playground"
                   "mperdikeas.github.com" "archetypes" "esavo-reg-II" "clojure-mutil"
-                  "mant-tasks" "starjava" "zip-diff" "esac-rawdar" "euler-js")
+                  "mant-tasks" "starjava" "zip-diff" "esac-rawdar" "euler-js"
+                  "js-react-minesweeper" "js-object-properties" "js-sort-enum"
+                  "js-filtered-datastore" "faker-datastore" "js-react-fixed-data-table-wrapper"
+                 )
