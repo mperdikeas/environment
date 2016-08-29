3e291f44660251041f6879f44ef12dbee8ab9ed7
mperdikeas <mperdikeas@gmail.com>
2016-08-29 10:57:51 +0300

    added new repos

--- repos-list.sh
+++ repos-list.sh
@@ -2,5 +2,6 @@ declare -a repos=("tools" "java-mutil" "environment" "ocaml-mutil" "playground"
                   "mperdikeas.github.com" "archetypes" "esavo-reg-II" "clojure-mutil"
                   "mant-tasks" "starjava" "zip-diff" "esac-rawdar" "euler-js"
                   "js-react-minesweeper" "js-object-properties" "js-sort-enum"
-                  "js-filtered-datastore" "faker-datastore" "js-react-fixed-data-table-wrapper"
+                  "js-filtered-datastore" "faker-datastore" "js-react-fixed-data-table-wrapper" "js-react-catch-the-lion"
+                  "js-geometry-2d"
                  )
