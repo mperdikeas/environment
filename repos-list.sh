7f58e8079ab2ed8e0ed8a4ca01d6aa18e34369e3
mperdikeas <mperdikeas@gmail.com>
2016-11-14 23:01:30 -0500

    foo

--- repos-list.sh
+++ repos-list.sh
@@ -3,5 +3,5 @@ declare -a repos=("tools" "java-mutil" "environment" "ocaml-mutil" "playground"
                   "mant-tasks" "starjava" "zip-diff" "esac-rawdar" "euler-js"
                   "js-react-minesweeper" "js-object-properties" "js-sort-enum"
                   "js-filtered-datastore" "faker-datastore" "js-react-fixed-data-table-wrapper" "js-react-catch-the-lion"
-                  "js-geometry-2d" "js-react-tetris"
+                  "js-geometry-2d" "js-react-tetris" "js-react-custom-validators"
                  )
