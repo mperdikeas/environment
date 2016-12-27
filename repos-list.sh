4ddb538fa20819003dd0d9fac773e42cd6a552fe
mperdikeas <mperdikeas@gmail.com>
2016-12-26 19:49:00 -0500

    added new repo (stack piling & tumbling)

--- repos-list.sh
+++ repos-list.sh
@@ -4,5 +4,5 @@ declare -a repos=("tools" "java-mutil" "environment" "ocaml-mutil" "playground"
                   "js-react-minesweeper" "js-object-properties" "js-sort-enum"
                   "js-filtered-datastore" "faker-datastore" "js-react-fixed-data-table-wrapper" "js-react-catch-the-lion"
                   "js-geometry-2d" "js-react-tetris" "js-react-custom-validators"
-                  "environment-sensitive" "js-structural-sharing" "js-string-tokenize" "cfa"
+                  "environment-sensitive" "js-structural-sharing" "js-string-tokenize" "cfa" "js-react-stack-piling-and-tumbling"
                  )
