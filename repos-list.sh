0410ad978bce89c08c3d2517a536d97900a69391
mperdikeas <mperdikeas@gmail.com>
2017-02-08 17:15:08 -0500

    fixed repos-list.sh

--- repos-list.sh
+++ repos-list.sh
@@ -4,6 +4,6 @@ declare -a repos=("tools" "java-mutil" "environment" "ocaml-mutil" "playground"
                   "js-react-minesweeper" "js-object-properties" "js-sort-enum"
                   "js-filtered-datastore" "faker-datastore" "js-react-fixed-data-table-wrapper" "js-react-catch-the-lion"
                   "js-geometry-2d" "js-react-tetris" "js-react-custom-validators"
-                  "environment-sensitive" "js-structural-sharing" "js-string-tokenize" "cfa" "js-react-stack-piling-and-tumbling"
+                  "environment-sensitive" "js-structural-sharing" "js-string-tokenize" "js-react-stack-piling-and-tumbling"
                   "js-fixed-size-lifo" "java-package-refactor" "emacs" "bashrc"
                  )
