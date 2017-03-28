a4384ad5502d6c41ef9f737a711929f507f4a164
mperdikeas <mperdikeas@gmail.com>
2017-03-28 09:23:32 -0400

    Remove mant-tasks project

--- repos-list.sh
+++ repos-list.sh
@@ -1,6 +1,6 @@
 declare -a repos=("tools" "java-mutil" "environment" "ocaml-mutil" "playground"
                   "mperdikeas.github.com" "archetypes" "clojure-mutil"
-                  "mant-tasks" "zip-diff" "euler-js"
+                   "zip-diff" "euler-js"
                   "js-react-minesweeper" "js-object-properties" "js-sort-enum"
                   "js-filtered-datastore" "faker-datastore" "js-react-fixed-data-table-wrapper" "js-react-catch-the-lion"
                   "js-geometry-2d" "js-react-tetris" "js-react-custom-validators"
