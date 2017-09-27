9357c93fc6df5c61958ee434634278fe10d783b0
mperdikeas <mperdikeas@gmail.com>
2017-09-27 10:26:38 -0400

    added js-ai-for-shogi-like-games

--- repos-list.sh
+++ repos-list.sh
@@ -1,7 +1,7 @@
 declare -a repos=("tools" "java-mutil" "environment" "ocaml-mutil" "playground"
                   "mperdikeas.github.com" "archetypes" "clojure-mutil"
                    "zip-diff" "euler-js"
-                  "js-react-minesweeper" "js-object-properties" "js-sort-enum"
+                  "js-react-minesweeper" "js-object-properties" "js-sort-enum" "js-ai-for-shogi-like-games"
                   "js-filtered-datastore" "faker-datastore" "js-react-fixed-data-table-wrapper" "js-react-catch-the-lion"
                   "js-geometry-2d" "js-react-tetris" "js-react-custom-validators"
                   "environment-sensitive" "js-structural-sharing" "js-string-tokenize" "js-react-stack-piling-and-tumbling"
