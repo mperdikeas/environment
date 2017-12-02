2c6c2d33e98a10e9481874d441357a5430455282
mperdikeas <mperdikeas@gmail.com>
2017-12-01 20:09:33 -0500

    added js-minmax-naive-but-instructive in repos list

--- repos-list.sh
+++ repos-list.sh
@@ -6,5 +6,5 @@ declare -a repos=("tools" "java-mutil" "environment" "ocaml-mutil" "playground"
                   "js-geometry-2d" "js-react-tetris" "js-react-custom-validators"
                   "environment-sensitive" "js-structural-sharing" "js-string-tokenize" "js-react-stack-piling-and-tumbling"
                   "js-fixed-size-lifo" "java-package-refactor" "emacs" "bashrc"
-                  "js-algebra" "js-trees" "js-flow-common-types" "js-minmax-wt-alpha-beta-pruning"
+                  "js-algebra" "js-trees" "js-flow-common-types" "js-minmax-wt-alpha-beta-pruning" "js-minmax-naive-but-instructive"
                  )
