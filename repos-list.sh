d42ff1dab10a764e35aa12427cb3b9312fbf4114
mperdikeas <mperdikeas@gmail.com>
2020-12-18 15:03:17 +0200

    changed ts-react-iacs location

--- repos-list.sh
+++ repos-list.sh
@@ -12,6 +12,6 @@ declare -a repos=("tools" "java-mutil" "environment" "ocaml-mutil" "playground"
                   "js-vibrations" "py-number-theory" "py-probability" "js-cubehelix" "js-cubehelix-demo"
                   "js-towers-of-hanoi" "archetype-webpack-babel-mocha-react" "archetype-java-ant-ivy" "archetype-JSON-web-tokens-JJWT" "digiterra"
                   "demo-spa-and-web-services-with-jwt-auth-type-01" "js-react-leaflet" "archetype-react-router" "archetype-js-grokking-rest-syntax"
-                  "archetype-ts-node-playground" "archetype-ts-antd-playground" "ts-react-iacs" "sscce-typescript-react-eslint"
+                  "archetype-ts-node-playground" "archetype-ts-antd-playground" "cognitera-iacs/ts-react-iacs" "sscce-typescript-react-eslint"
                   "playground-ts-node-promises" "antd-form-list-sscce" "openlayers-playground"
                  )
