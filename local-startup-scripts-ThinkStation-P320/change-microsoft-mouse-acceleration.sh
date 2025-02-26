39bf9cc43c304cb0864c4b64a523be6e3ae13f09
mperdikeas <mperdikeas@gmail.com>
2025-02-26 11:49:45 +0200

    updated script to change mouse acceleration

--- local-startup-scripts-ThinkStation-P320/change-microsoft-mouse-acceleration.sh
+++ local-startup-scripts-ThinkStation-P320/change-microsoft-mouse-acceleration.sh
@@ -9,4 +9,10 @@
 #
 #        @reboot /home/mperdikeas/repos/prj/environment/local-startup-apps-ThinkStation-P320/change-microsoft-mouse-acceleration.sh
 #
-xinput --set-prop 10 340 1
+xinput --set-prop 14 340 1
+
+
+# https://chat.deepseek.com/a/chat/s/255f01aa-d93c-4957-addd-52305269dcaa
+
+# $ xinput list
+# $ xinput list props <device-id>
